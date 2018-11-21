ffc1d6246bef13efb047f69a37f54ed0f49084a7
Jack Ross <mrjackross@gmail.com>
2018-11-21 15:53:59 -0500

    To the point where it's able to run AWS CLI commands

--- s3-media-library.php
+++ s3-media-library.php
@@ -3,10 +3,11 @@
 Plugin Name: S3/Media Library Syncer
 Plugin URI: http://www.pcommsites.com
 Description: Plugin for syncing the Media Library to match files in S3.
-Version: 0.0.1
+Version: 0.0.2
 Author: PartnerComm
 */
 
+register_setting( 'S3/Media Library Syncer', 's3_media_library_syncer_options_page' );
 // Add the admin page
 add_action('admin_menu', 's3_media_library_syncer_add_page', 11);
 
@@ -16,12 +17,40 @@ function s3_media_library_syncer_add_page() {
 }
 
 function s3_media_library_syncer_options_page() {
+    $display = "Click run!";
+    if($_POST) {
+        if(!empty($_POST['runS3MediaSyncer'])) {
+            if(defined("AS3CF_SETTINGS")) {
+                $settings = unserialize(AS3CF_SETTINGS);
+                if(!empty($settings['provider']) && $settings['provider'] == 'aws') {
+                    if(
+                        !empty($settings['access-key-id']) &&
+                        !empty($settings['secret-access-key'])
+                    ) {
+                        $controller = new \PComm\S3MediaLibrary\Controller();
+                        $display = $controller->run($settings);
+                    } else {
+                        $display = "Key ID and Access Key not provided";
+                    }
+                } else {
+                    $display = "Need to use AWS as provider";
+                }
+            } else {
+                $display = "Need to add settings for AWS/S3 Plugin!!!";
+            }
+        }
+    }
     ?>
 
     <div class="wrap">
         <?php screen_icon(); ?>
         <h2>S3/Media Library Syncer</h2>
-        Testing this
+        <div>
+            <form method="post">
+                <input name="runS3MediaSyncer" class="button-primary" type="submit" value="Run Syncer"/>
+            </form>
+        </div>
+        <textarea rows="4" cols="100" disabled><?=$display?></textarea>
     </div>
 
     <?php
--- src/Controller.php
+++ src/Controller.php
@@ -3,4 +3,47 @@
 namespace PComm\S3MediaLibrary;
 
 class Controller {
+
+    protected $result = "Starting...\n";
+    protected $settings;
+    protected $bucketData;
+
+    public function run($settings) {
+        try {
+            $this->settings = $settings;
+            $this->bucketData = $this->getBucketData();
+            $this->getFiles();
+        } catch (\Exception $e) {
+            $this->result .= "Encountered unhandled error: ". $e->getMessage();
+        }
+        return $this->result;
+    }
+
+    protected function getFiles() {
+        $command = $this->getCredentialPrefix();
+        $command .= "aws s3 ls";
+        var_dump(shell_exec($command));
+    }
+
+    protected function getCredentialPrefix() {
+        $command = "AWS_ACCESS_KEY_ID={$this->settings['access-key-id']} ";
+        $command .= "AWS_SECRET_ACCESS_KEY={$this->settings['secret-access-key']} ";
+        return $command;
+    }
+
+    protected function getBucketData() {
+        $settings = get_option('_site_transient_as3cf_regions_cache');
+        if(empty($settings)) {
+            throw new \Exception("Bucket name not set");
+        }
+        var_dump($settings);
+        $bucketData = new \stdClass();
+        foreach($settings as $name => $region) {
+            $bucketData->name = $name;
+            $bucketData->region = $region;
+            // There should only be one result
+            break;
+        }
+        return $bucketData;
+    }
 }
